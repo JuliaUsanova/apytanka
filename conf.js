afa6c5fd5987e2ce84e03180ab7bad469604c6a7
julia usanova <juliausanova@gmail.com>
2014-11-19 11:26:49 +0200

    protractor config file

---
+++ conf.js
@@ -0,0 +1,13 @@
+/**
+ * Created by юля on 14.11.2014.
+ */
+exports.config = {
+    framework: 'jasmine',
+//    directConnect: true,
+    capabilities: {
+        browserName: 'firefox'
+    },
+    chromeDriver: 'C:/Users/юля/AppData/Roaming/npm/node_modules/protractor/selenium/chromedriver',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['test/e2e/*_spec.js']
+};
