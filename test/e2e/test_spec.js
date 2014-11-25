9006f301f1a5f396df8bad1df19502e539155310
julia usanova <juliausanova@gmail.com>
2014-11-25 12:15:21 +0200

    tests directory

---
+++ test/e2e/test_spec.js
@@ -0,0 +1,13 @@
+/**
+ * Created by юля on 14.11.2014.
+ */
+describe('homepage', function() {
+    beforeEach(function() {
+        // before function
+    });
+
+    it('should load the page', function() {
+        // test goes here
+        expect('hello').toEqual('hello');
+    });
+});
---
+++ test/unit/unit1.js
@@ -0,0 +1,11 @@
+/**
+ * Created by юля on 16.11.2014.
+ */
+define('', function(){
+
+    it('', function(){
+
+
+    });
+
+});
