757a6b601019826823056d5a8a848485488b15cd
julia usanova <juliausanova@gmail.com>
2014-11-26 14:17:07 +0200

    user controllers with user class

--- app/js/user/userControllers.js
+++ app/js/user/userControllers.js
@@ -3,10 +3,50 @@
  */
 
 (function(){
+
+    var User;
+
+    User = (function(){
+        function UserClass ( data ) {
+            this.id = data.id;
+            this.name = data.name;
+            this.surname = data.surname;
+            this.email = data.email;
+            this.gender = data.gender;
+            this.dateOfBirth = new Date(data.dateOfBirth);
+            this.country = data.country;
+            this.city = data.city;
+            this.street = data.street;
+            this.skills = data.skills || [{value: '', name: ''}];
+            this.interests = data.interests || [{value: '', descr: ''}];
+            this.avatar = (data.avatar);
+
+        }
+
+        UserClass.prototype.addNewSkill = function(){
+            this.skills.push({});
+        };
+
+        UserClass.prototype.addNewInterest = function(){
+            this.interests.push({});
+        };
+
+        UserClass.prototype.removeSkill = function(skill){
+            var index = this.skills.indexOf(skill);
+            this.skills.splice(index, 1);
+        };
+
+        UserClass.prototype.removeInterest = function(interest){
+            var index = this.interests.indexOf(interest);
+            this.interests.splice(index, 1);
+        };
+
+        return UserClass;
+    })();
+
     var userControllers = angular.module('userControllers', []);
 
     userControllers.controller('profileCtrl', ['$scope', function($scope){
-        var User;
         var userData = {
             id: 1, name: 'User', surname: 'Userov', email: 'user@gmail.com', avatar: '../css/images/563469251.png', gender: 'f',
             dateOfBirth: 634600800000, country: 'Belarus', city: 'Minsk', street: 'Russianova',
@@ -16,44 +56,6 @@
             ],
             interests: [{value: 0, descr: 'tra ta ta'},{value: 1, descr: 'like to listen the music'},{value: 4, descr: 'do sport every day'},{value: 5, descr: ''}]
         };
-
-        User = (function(){
-            function UserClass ( data ) {
-                this.id = data.id;
-                this.name = data.name;
-                this.surname = data.surname;
-                this.email = data.email;
-                this.gender = data.gender;
-                this.dateOfBirth = new Date(data.dateOfBirth);
-                this.country = data.country;
-                this.city = data.city;
-                this.street = data.street;
-                this.skills = data.skills || [{value: '', name: ''}];
-                this.interests = data.interests || [{value: '', descr: ''}];
-                this.avatar = (data.avatar);
-
-            }
-
-            UserClass.prototype.addNewSkill = function(){
-                this.skills.push({});
-            };
-
-            UserClass.prototype.addNewInterest = function(){
-                this.interests.push({});
-            };
-
-            UserClass.prototype.removeSkill = function(skill){
-                var index = this.skills.indexOf(skill);
-                this.skills.splice(index, 1);
-            };
-
-            UserClass.prototype.removeInterest = function(interest){
-                var index = this.interests.indexOf(interest);
-                this.interests.splice(index, 1);
-            };
-
-            return UserClass;
-        })();
 
 
         $scope.selectFile = function(){
