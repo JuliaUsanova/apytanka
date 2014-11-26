c30c83f3ff1bb4c29515efcdc0a7895b61617317
julia usanova <juliausanova@gmail.com>
2014-11-26 17:27:00 +0200

    User service added to the controller

--- app/js/user/userControllers.js
+++ app/js/user/userControllers.js
@@ -4,49 +4,9 @@
 
 (function(){
 
-    var User;
-
-    User = (function(){
-        function UserClass ( data ) {
-            this.id = data.id;
-            this.name = data.name;
-            this.surname = data.surname;
-            this.email = data.email;
-            this.gender = data.gender;
-            this.dateOfBirth = new Date(data.dateOfBirth);
-            this.country = data.country;
-            this.city = data.city;
-            this.street = data.street;
-            this.skills = data.skills || [{value: '', name: ''}];
-            this.interests = data.interests || [{value: '', descr: ''}];
-            this.avatar = (data.avatar);
-
-        }
-
-        UserClass.prototype.addNewSkill = function(){
-            this.skills.push({});
-        };
-
-        UserClass.prototype.addNewInterest = function(){
-            this.interests.push({});
-        };
-
-        UserClass.prototype.removeSkill = function(skill){
-            var index = this.skills.indexOf(skill);
-            this.skills.splice(index, 1);
-        };
-
-        UserClass.prototype.removeInterest = function(interest){
-            var index = this.interests.indexOf(interest);
-            this.interests.splice(index, 1);
-        };
-
-        return UserClass;
-    })();
-
     var userControllers = angular.module('userControllers', []);
 
-    userControllers.controller('profileCtrl', ['$scope', function($scope){
+    userControllers.controller('profileCtrl', ['$scope', 'User', function($scope, User){
         var userData = {
             id: 1, name: 'User', surname: 'Userov', email: 'user@gmail.com', avatar: '../css/images/563469251.png', gender: 'f',
             dateOfBirth: 634600800000, country: 'Belarus', city: 'Minsk', street: 'Russianova',
@@ -73,7 +33,7 @@
                     reader.readAsDataURL(file);
 
                     reader.onload = function(e) {
-                        $scope.user.avatar = (e.target.result);
+                        $scope.currentUser.avatar = (e.target.result);
                     };
 
 
@@ -111,7 +71,7 @@
             {value: 8, name: "Турызм"}, {value: 9, name: "Дызайн"}];
 
 
-        $scope.user = new User(userData);
+        $scope.currentUser = new User(userData);
 
 
 
