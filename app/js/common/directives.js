5df55a8bbc484245b9501b21e2c8528e1d996a2a
julia usanova <juliausanova@gmail.com>
2014-12-15 15:41:11 +0300

    includeReplace added

--- app/js/common/directives.js
+++ app/js/common/directives.js
@@ -28,4 +28,14 @@
         };
     }]);
 
+    app.directive('includeReplace', function () {
+        return {
+            require: 'ngInclude',
+            restrict: 'A',
+            link: function (scope, el, attrs) {
+                el.replaceWith(el.children());
+            }
+        };
+    });
+
 })();
