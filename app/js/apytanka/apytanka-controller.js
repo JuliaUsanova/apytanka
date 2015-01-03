ede4c2843265011caba884e06b855a92c43c445e
julia usanova <juliausanova@gmail.com>
2015-01-03 10:04:44 +0300

    apytankaListCtrl cleared

--- app/js/apytanka/apytanka-controller.js
+++ app/js/apytanka/apytanka-controller.js
@@ -4,24 +4,9 @@
 (function(){
     var apytanka = angular.module('apytanka');
 
-    apytanka.controller('apytankaListCtrl', ['$scope', 'ApytankaFactory', function($scope, ApytankaFactory){
+    apytanka.controller('apytankaListCtrl', ['$scope', 'ApytankaFactory', '$location', function($scope, ApytankaFactory, $location){
 
-        $scope.apytankaList = [];
 
-        var apytankaList = [
-            {id: '1', user: {name: 'Ivan', surname: 'Ivanov', id: 123, avatar: 'css/images/563469251.png', country: 'Thailand', city: 'Bangkok'}, title: "First", content: 'bla-bla-bla',
-                date: 634600801000, rating: {likes: [2, 12, 125, 45, 85, 44], dislikes: [14,58,21]}},
-            {id: '2', user: {name: 'Julian', surname: 'Cesar', id: 123, avatar: 'css/images/563469251.png', country: 'Italy', city: 'Rome'}, title: "AHAHAHAH",
-                content: 'Fortune, which has a great deal of power in other matters but especially in war, can bring about great changes in a situation through very slight forces',
-                date: 634600801001, rating: {likes: [14, 12, 115, 15, 84, 42,64], dislikes: [18,88,26,53]}},
-            {id: '3', user: {name: 'Kiloak', surname: 'Kiser', id: 123, avatar: 'css/images/563469251.png', country: 'Germany', city: 'Berlin'}, title: "fsdadfg",
-                content: 'What we wish, we readily believe, and what we ourselves think, we imagine others think also',
-                date: 634600801401, rating: {likes: [1, 12, 10, 13, 8], dislikes: [18]}}
-        ];
-
-        angular.forEach(apytankaList, function(obj) {
-            this.push(new ApytankaFactory(obj));
-        }, $scope.apytankaList);
 
     }]);
 
