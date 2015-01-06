71853292f7f4a534bfd3985ae776342dd6e0b6f4
julia usanova <juliausanova@gmail.com>
2015-01-06 18:17:58 +0300

    commented apytankaListCtrl

--- app/js/apytanka/apytanka-controller.js
+++ app/js/apytanka/apytanka-controller.js
@@ -4,11 +4,81 @@
 (function(){
     var apytanka = angular.module('apytanka');
 
-    apytanka.controller('apytankaListCtrl', ['$scope', 'ApytankaFactory', '$location', function($scope, ApytankaFactory, $location){
-
-
-
-    }]);
+//    apytanka.controller('apytankaListCtrl', ['$scope', '$location', 'filterService', function($scope, $location, filterService){
+//
+//        $scope.filterParams = {
+//            byCountries: {
+//                list: [{id: 'all', name: 'Усе'}],
+//                selected: 'all'
+//            },
+//            search: '',
+//            byDate: {
+//                options: [{value: 0, name: 'Спачатку новыя'}, {value: 1, name: 'Спачатку старыя'}, {value: 'all', name: 'Не мае сэнса'}],
+//                selected: 'all'
+//            },
+//            byRating: {
+//                options: [{value: 0, name: 'Спачатку з вышэйшым'}, {value: 1, name: 'Спачатку з ніжэйшым'}, {value: 'all', name: 'Не мае сэнса'}],
+//                selected: 'all'
+//            }
+//        };
+//
+//        $scope.$watch($location, function(newVal, oldVal){
+////                filterService.list('apytanki', $location.search()).success(function(data){
+////                                $scope.filteredList = data;
+////                              for ( var i = 0; i < data.length; i++ ) {
+////                              if( $scope.filterParams.byCountries.list.indexOf(data[i].user.country) != -1 ) return;
+////                              $scope.filterParams.byCountries.list.push(data[i].user.country);
+////                              };
+////                            });
+//            $scope.filteredList = filterService.list('apytanki', $location.search());
+//            for ( var i = 0; i < $scope.filteredList.length; i++ ) {
+//                if( $scope.filterParams.byCountries.list.indexOf($scope.filteredList[i].user.country) != -1 ) return;
+//                $scope.filterParams.byCountries.list.push($scope.filteredList[i].user.country);
+//            };
+//        }, true);
+//
+//
+//
+//
+//
+////        $scope.getData = function(){
+////            var params;
+////            var requestMethod;
+////
+////            var parseUrl = function(url){
+////                requestMethod = $location.path();
+////                params = $location.search();
+////            };
+////            return function($routeParams){
+////                parseUrl(url);
+//////                            return $http({
+//////                                method: requestMethod,
+//////                                url: 'https:...' + params
+//////                            }).success(function(data){
+//////                                $scope.filteredList = data;
+//////                              for ( var i = 0; i < data.length; i++ ) {
+//////                              if( $scope.filters.byCountries.list.indexOf(data[i].user.country) != -1 ) return;
+//////                              $scope.filters.byCountries.list.push(data[i].user.country);
+//////                              };
+//////                            });
+////
+////                $scope.filteredList = [
+////                    {id: '1', user: {name: 'Ivan', surname: 'Ivanov', id: 123, avatar: 'css/images/563469251.png', country: {id: 2, name: 'Thailand'}, city: 'Bangkok'}, title: "First", content: 'bla-bla-bla',
+////                        date: 634600801000, rating: {likes: [2, 12, 125, 45, 85, 44], dislikes: [14,58,21]}},
+////                    {id: '2', user: {name: 'Julian', surname: 'Cesar', id: 123, avatar: 'css/images/563469251.png', country: {id: 1, name: 'Italy'}, city: 'Rome'}, title: "AHAHAHAH",
+////                        content: 'Fortune, which has a great deal of power in other matters but especially in war, can bring about great changes in a situation through very slight forces',
+////                        date: 634600801001, rating: {likes: [14, 12, 115, 15, 84, 42,64], dislikes: [18,88,26,53]}},
+////                    {id: '3', user: {name: 'Kiloak', surname: 'Kiser', id: 123, avatar: 'css/images/563469251.png', country: {id: 3, name: 'Germany'}, city: 'Berlin'}, title: "fsdadfg",
+////                        content: 'What we wish, we readily believe, and what we ourselves think, we imagine others think also',
+////                        date: 634600801401, rating: {likes: [1, 12, 10, 13, 8], dislikes: [18]}}
+////                ];
+////            };
+////
+////        };
+////
+////        $scope.getData();
+//
+//    }]);
 
     apytanka.controller('apytankaCtrl', ['$scope', 'ApytankaFactory', 'userService', function($scope, ApytankaFactory, userService){
 
