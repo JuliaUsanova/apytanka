317aceb841eac05ac3f87a7cc4406d2bedb24ab7
julia usanova <juliausanova@gmail.com>
2015-01-08 20:24:11 +0300

    filter directive modified

--- app/js/common/directives.js
+++ app/js/common/directives.js
@@ -65,14 +65,15 @@
        return{
            restrict: 'EA',
            templateUrl: '../partials/filter.html',
-           controller: ['$scope', '$location', 'filterService', function($scope, $location, filterService){
+           controller: ['$scope', '$location', function($scope, $location){
 
                $scope.filterParams = {
                    byCountries: {
                        list: [{id: 'all', name: 'Усе'}],
                        selected: 'all'
                    },
-                   search: '',
+                   search: {selected: ''},
+                   page: {selected: 1},
                    byDate: {
                        options: [{value: 0, name: 'Спачатку новыя'}, {value: 1, name: 'Спачатку старыя'}, {value: 'all', name: 'Не мае сэнса'}],
                        selected: 'all'
@@ -83,58 +84,35 @@
                    }
                };
 
-               var location = $location.search();
+               $scope.filterBy = {'country': 'all', 'search': '', 'page': 1, date: 'all', rating: 'all'};
 
-               var locationParams = Object.keys(location);
 
-               for ( var i=0; i < locationParams.length; i++ ) {
-                   if (locationParams[i] == 'search' && location[locationParams[i]] != '') {
-                       $scope.filterParams[locationParams[i]] = location[locationParams[i]];
-                   }
-                   else if (locationParams[i] != 'search' && typeof(location[locationParams[i]]) == 'number') {
-                       $scope.filterParams[locationParams[i]].selected = location[locationParams[i]];
-                   }
-               }
-
-               $scope.updateLocation = function (obj){
-
-                   var searchObj = {};
-
-                   var keyArr = Object.keys(obj);
+               var setParams = function (obj1, obj2){
+                   var keyArr = Object.keys(obj1);
                    for ( var i=0; i < keyArr.length; i++ ) {
-                       if (keyArr[i] == 'search' && obj[keyArr[i]] != '') {
-                           searchObj[keyArr[i]] = obj[keyArr[i]];
+                       if (keyArr[i] == 'search' && obj1[keyArr[i]] != '') {
+                           obj2[keyArr[i]] = obj1[keyArr[i]];
                        }
-                       else if (keyArr[i] != 'search' && typeof(obj[keyArr[i]]['selected']) == 'number') {
-                           searchObj[keyArr[i]] = obj[keyArr[i]].selected;
+                       else if (keyArr[i] != 'search' && !isNaN(parseInt(obj1[keyArr[i]])) ) {
+                           obj2[keyArr[i]] = obj1[keyArr[i]];
                        }
                    }
-                   if ( jQuery.isEmptyObject(searchObj) ) searchObj['selected'] = 'all';
-
-                   $location.search(searchObj);
-
                };
 
-               $scope.$watch($location, function(newVal, oldVal){
-//                filterService.list('apytanki', $location.search()).success(function(data){
-//                                $scope.filteredList = data;
-//                              for ( var i = 0; i < data.length; i++ ) {
-//                              if( $scope.filterParams.byCountries.list.indexOf(data[i].user.country) != -1 ) return;
-//                              $scope.filterParams.byCountries.list.push(data[i].user.country);
-//                              };
-//                            });
-                   $scope.filteredList = filterService.list('apytanki', $location.search());
-                   for ( var i = 0; i < $scope.filteredList.length; i++ ) {
-                       if( $scope.filterParams.byCountries.list.indexOf($scope.filteredList[i].user.country) != -1 ) return;
-                       $scope.filterParams.byCountries.list.push($scope.filteredList[i].user.country);
-                   };
-               }, true);
+               setParams($location.search(), $scope.filterBy);
+               if ( jQuery.isEmptyObject($location.search()) ) $location.search({page: 1});
+
+               $scope.updateLocation = function (urlObj){
+                   var filtersObj = {};
+                   setParams(urlObj, filtersObj);
+                   jQuery.isEmptyObject(filtersObj) ? $location.search({page: 1}) : $location.search(filtersObj);
+               };
 
            }],
            link: function($scope, iElement, iAttrs, ctrl){
 
-               $scope.$watch('filterParams', function(newVal, oldVal){
-                   if ( newVal != oldVal ) $scope.updateLocation(newVal);
+               $scope.$watch('filterBy', function(newVal, oldVal){
+                   if ( !angular.equals(newVal, oldVal) ) $scope.updateLocation(newVal);
                }, true);
 
 
